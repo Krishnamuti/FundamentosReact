7802ad388df047eef0b5837e7d20111b20f61df7
Krishnamuti <krishnamuti_2009@hotmail.com>
2020-10-05 23:43:12 -0300

    Componente com Estado

--- FRONT/src/components/comunicacao/IndiretaFilho.jsx
+++ FRONT/src/components/comunicacao/IndiretaFilho.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-export default props => {    
+export default props => {
+    const gerarIdade = () => parseInt(Math.random() * (20)) + 50;    
     return(
         <div>
             <div>Filho</div>
             <button onClick={
                 function (e) {
-                    props.quandoClicar('João', 53, true);
+                    props.quandoClicar('João', gerarIdade(), true);
                 }
             }>Fornecer Informações</button>
         </div>        
