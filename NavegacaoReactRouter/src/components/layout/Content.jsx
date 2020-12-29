a148148d6717b081c1fcd894f7c5b71d98a7f884
Krishnamuti <krishnamuti_2009@hotmail.com>
2020-12-28 23:30:26 -0300

    página não encontrada.

--- NavegacaoReactRouter/src/components/layout/Content.jsx
+++ NavegacaoReactRouter/src/components/layout/Content.jsx
@@ -5,6 +5,7 @@ import { Switch, Route, Router } from "react-router-dom";
 import Home from "../../views/examples/Home";
 import Param from "../../views/examples/Param";
 import About from "../../views/examples/About";
+import NotFound from "../../views/examples/NotFound";
 
 const Content = props => (
     <main className="Content">
@@ -15,8 +16,11 @@ const Content = props => (
             <Route path="/param/:id">
                 <Param />
             </Route>
-            <Route path="/">
+            <Route exact path="/">
                 <Home />
+            </Route>
+            <Route path="*">
+                <NotFound />
             </Route>
         </Switch>
     </main>
